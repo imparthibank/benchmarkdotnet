968eed5e9f06d26791ca146ae80c7ad4ce8001d5
Parthiban <kparthimsc@gmail.com>
2024-07-10 01:19:38 +0530

    Added Benchmark Playground Project

---
+++ benchmarkdotnet/Benchmarking.Benchmarkdotnet.Playground/BenchmarkHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarking.Benchmarkdotnet.Playground
+{
+    [MemoryDiagnoser]
+    [ShortRunJob]
+    public class BenchmarkHelper
+    {
+        List<int>? _list;
+        [Params(1_000, 10_000,1_00_000,10_00_000)]        
+        public int ListSize;
+        [GlobalSetup]
+        public void Setup()
+        {
+            _list = new List<int>();
+            for (int i = 0; i < ListSize; i++)
+            {
+                _list.Add(i);
+            }
+        }
+        [Benchmark]
+        public void SortByNumbers()
+        {
+
+            _list!.Sort();
+        }
+
+    }
+}
---
+++ benchmarkdotnet/Benchmarking.Benchmarkdotnet.Playground/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using Benchmarking.Benchmarkdotnet.Playground;
+using System.Reflection;
+
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
+BenchmarkHelper benchmarkHelper = new();
+benchmarkHelper.SortByNumbers();
